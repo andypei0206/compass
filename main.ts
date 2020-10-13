a7edeb273bd04dc94fd85003b521933fb9a805ca
andypei0206 <71689509+andypei0206@users.noreply.github.com>
2020-10-13 14:56:07 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let compass = 0
+basic.forever(function () {
+    compass = input.compassHeading()
+    if (compass <= 45 && compass >= 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (compass > 315 && compass <= 359) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (compass <= 315 && compass > 225) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (compass <= 225 && compass > 135) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (compass <= 135 && compass > 45) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
